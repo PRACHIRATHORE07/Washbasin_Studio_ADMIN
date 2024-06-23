2f6a22cddbcefeb0bf587054b61cdc317eb6edaf
bhartendra-geek <bhartendrasharma780733@gmail.com>
2024-06-23 23:30:13 +0530

    title and logo

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -7,8 +7,8 @@ import { ClerkProvider } from "@clerk/nextjs";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Borcelle - Admin Auth",
-  description: "Admin dashboard to manage Borcelle's data",
+  title: "THE WASHBASIN STUDIO - Admin Auth",
+  description: "Admin dashboard to manage TWS's data",
 };
 
 export default function RootLayout({
